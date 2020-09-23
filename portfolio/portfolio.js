197bd79cd08f6f04ca41c6c4ae615ea747b4aa5b
Aparna S <aparna.sathyan.as@gmail.com>
2020-09-23 16:29:27 +0530

    updates

--- portfolio/portfolio.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector('.hamburger-menu');
-const hambar = document.querySelector('.ham-bar');
-function handleClick(){
-    hambar.classList.toggle("change");
-}
-menuIcon.addEventListener('click', handleClick);
