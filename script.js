9709c473eb30b283a8adb81164dd2bf3099a7957
Aparna S <aparna.sathyan.as@gmail.com>
2020-09-23 16:13:47 +0530

    updates

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menuIcon = document.querySelector('.hamburger-menu');
 const hambar = document.querySelector('.ham-bar');
-menuIcon.addEventListener('click', ()=> {
+function handleClick(){
     hambar.classList.toggle("change");
-});
+}
+menuIcon.addEventListener('click', handleClick);
